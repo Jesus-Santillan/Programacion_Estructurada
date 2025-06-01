c7b09058df2d4e39453080f40fe758638982e63f
Jesus-Santillan <jesus_3041240414@utd.edu.mx>
2025-06-01 16:08:04 -0600

    Delete P1/5_Dict directory

--- P1/5_Dict/dict.py
+++
@@ -1,5 +0,0 @@
-'''
-dict.-
-Es un tipo de dato que se utiliza para almacenar datos de diferente tipo de datos pero en lugar
-de tener como laslistas un indice numerico tiene alfanumericos. Es decir, es algo parecido a los objetos.
-'''
